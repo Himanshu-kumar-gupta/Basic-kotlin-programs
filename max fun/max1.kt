acc3e062d044c9f3be3d2f321c0b125256a4331a
Himanshu-kumar-gupta <60381467+Himanshu-kumar-gupta@users.noreply.github.com>
2021-02-22 21:03:45 +0530

    Program 3 ---> Expression bodies in functions.
    This folder explains expression bodies in functions.See the programs in order     max1->max2->max3      to understand.

---
+++ max fun/max1.kt
@@ -0,0 +1,9 @@
+fun max(a:Int,b:Int):Int
+{
+    return if(a>b)a else b
+}
+
+fun main(args:Array<String>)
+{
+    println("Max = "+max(23,45))
+}
---
+++ max fun/max3.kt
@@ -0,0 +1,6 @@
+fun max(a:Int,b:Int)=if(a>b)a else b
+
+fun main(args:Array<String>)
+{
+    println("Max = "+max(23,45))
+}
