2eee3d6b3a85517bace299887b145af936e52bd0
Himanshu-kumar-gupta <60381467+Himanshu-kumar-gupta@users.noreply.github.com>
2021-02-22 21:22:38 +0530

    Program 4 ---> val and var
    This program explains val and var.

---
+++ variable.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>)
+{
+    val a:Int=5;
+    var b:Int=5;
+    println("Value of a= $a b= $b ")
+
+    //a=10;  val variable's value can't be changed once assigned
+    b=20;
+    println("Value of a= $a b= $b ")
+}
