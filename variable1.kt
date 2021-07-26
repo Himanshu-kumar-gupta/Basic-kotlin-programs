83e43b274fcf42fee8b11459b87fa10e798ca2a9
Himanshu-kumar-gupta <60381467+Himanshu-kumar-gupta@users.noreply.github.com>
2021-07-27 00:48:17 +0530

    Program 5 ---> assignment through expression
    This program demonstrates how different value can be assigned to val  variable through expression

---
+++ variable1.kt
@@ -0,0 +1,13 @@
+fun main(arg:Array<String>)
+{
+    val a=5
+    val b=5
+
+    val mess=    //diff value can be assigned to val , through expression 
+    if(a==b)
+      "Match"
+    else
+     "No match"
+
+     println(mess)
+}
