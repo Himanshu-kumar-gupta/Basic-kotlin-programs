b631aab54505719d7330ba183ab5bb7d98424a99
Himanshu-kumar-gupta <60381467+Himanshu-kumar-gupta@users.noreply.github.com>
2021-07-27 11:18:29 +0530

    Nullable with non-nullable variables
    Nullable with non-nullable variables compatibility

---
+++ strln.kt
@@ -0,0 +1,11 @@
+fun strlen(s:String)=s.length
+
+fun main(args:Array<String>)
+{
+    var n="golem"
+    val k:String?="skls"
+
+    // k.length  //cannot call library functions on nullable variables(use safeCall)
+    // n=k  //typemismatch - cannot assign value of simple variable to nullable variable 
+    println("$n = ${strlen(n)}")
+}
