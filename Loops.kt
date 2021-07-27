1fef3354b29ba3047e54165ef351e4e5c56c15e5
Himanshu-kumar-gupta <60381467+Himanshu-kumar-gupta@users.noreply.github.com>
2021-07-27 11:44:18 +0530

    Program 11 ---> while & do-while loops
     while & do-while loops are same as java

---
+++ Loops.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    // While and do-while loops are same as java without ;
+
+    println("\nWhile loop : ")
+    var i = 1
+    while(i<=10)
+        println(i++)
+
+    println("\ndo-While loop : ")
+    i=1
+    do {
+        println(i*2)
+         i++
+    }while (i<=10)
+}
